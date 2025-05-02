693b9cbcdc159a12ffe36573d62e223b9032015d
wahyu_07 <wahyuhidayatpku15@gmail.com>
2025-05-02 12:09:29 +0700

    Hapus node_modules dan .env dari tracking Git

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,25 @@
 const { Pool } = require('pg');
+const { Client } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',        
-  host: 'localhost',
+  host: '192.168.1.43',
   database: 'db_peminjaman_buku', 
   password: '7325',       
   port: 5432,                     
 });
+
+const client = new Client({
+  host: '192.168.1.43',  // IP server PostgreSQL
+  user: 'isanbegal',
+  password: 'isanbegal',
+  database: 'db_peminjaman_buku',
+  port: 5432
+});
+
+client.connect()
+  .then(() => console.log('Database connected!'))
+  .catch(err => console.log('Database connection error', err));
 
 pool.connect()
   .then(() => console.log('Koneksi ke database PostgreSQL berhasil'))
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
@@ -17,6 +19,24 @@ app.use('/api', anggotaRoutes);
 app.use('/api', bukuRoutes);
 app.use('/api', peminjamanRoutes);
 
+const pool = new Pool({
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,    
+  user: process.env.PGUSER, 
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE  
+});
+
+pool.connect()
+  .then(client => {
+    console.log('Terhubung ke database PostgreSQL');
+    client.release(); 
+  })
+  .catch(err => {
+    console.error('Gagal terhubung ke database PostgreSQL', err.stack);
+  });
+
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
+
